7a6f3ae25cc2d110fb731d0b529391ca5bef1f4e
Mikhail <kolosov.ma@phystech.edu>
2023-04-07 23:05:40 +0300

    add checks, format names

--- scripts/networkdata_ddl.sql
+++ scripts/networkdata_ddl.sql
@@ -1,104 +1,104 @@
 CREATE SCHEMA flq;
 
--- Create the Users table --
-CREATE TABLE flq.Users (
-  ID SERIAL PRIMARY KEY,
-  ProfilePhotoID INT REFERENCES flq.Photos(ID),
-  Name VARCHAR(255) NOT NULL,
-  Email VARCHAR(255) NOT NULL,
-  Password VARCHAR(255) NOT NULL,
-  Bio TEXT,
-  DateCreated TIMESTAMP NOT NULL
+-- Create the User table --
+CREATE TABLE flq.User (
+  id SERIAL PRIMARY KEY,
+  profile_photo_id INT REFERENCES flq.Photo(id),
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+  password VARCHAR(255) NOT NULL,
+  bio TEXT,
+  creation_date TIMESTAMP NOT NULL
 );
 
--- Create the Photos table --
-CREATE TABLE flq.Photos (
-  ID SERIAL PRIMARY KEY,
-  UserID INT NOT NULL REFERENCES flq.Users(ID),
-  Title VARCHAR(100) NOT NULL,
-  Description TEXT,
-  ImageURL TEXT NOT NULL,
-  DateUploaded TIMESTAMP NOT NULL
+-- Create the Photo table --
+CREATE TABLE flq.Photo (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES flq.User(id),
+  title VARCHAR(100) NOT NULL,
+  description TEXT,
+  image_url TEXT NOT NULL,
+  upload_date TIMESTAMP NOT NULL
 );
 
--- Create the Tags table --
-CREATE TABLE flq.Tags (
-  ID SERIAL PRIMARY KEY,
-  Name VARCHAR(100) NOT NULL
+-- Create the Tag table --
+CREATE TABLE flq.Tag (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
 );
 
--- Create the Likes table --
-CREATE TABLE flq.Likes (
-  ID SERIAL PRIMARY KEY,
-  UserID INT NOT NULL REFERENCES flq.Users(ID),
-  PhotoID INT NOT NULL REFERENCES flq.Photos(ID)
+-- Create the Like table --
+CREATE TABLE flq.Like (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES flq.User(id),
+  photo_id INT NOT NULL REFERENCES flq.Photo(id)
 );
 
--- Create the Comments table --
-CREATE TABLE flq.Comments (
-  ID SERIAL PRIMARY KEY,
-  UserID INT NOT NULL REFERENCES flq.Users(ID),
-  PhotoID INT NOT NULL REFERENCES flq.Photos(ID),
-  Comment TEXT NOT NULL,
-  DateCommented TIMESTAMP NOT NULL
+-- Create the Comment table --
+CREATE TABLE flq.Comment (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES flq.User(id),
+  photo_id INT NOT NULL REFERENCES flq.Photo(id),
+  comment TEXT NOT NULL,
+  comment_date TIMESTAMP NOT NULL
 );
 
--- Create the Albums table --
-CREATE TABLE flq.Albums (
-  ID SERIAL PRIMARY KEY,
-  Title VARCHAR(100) NOT NULL,
-  Description TEXT,
-  Privacy VARCHAR(10) NOT NULL DEFAULT 'public',
-  DateCreated TIMESTAMP NOT NULL
+-- Create the Album table --
+CREATE TABLE flq.Album (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  description TEXT,
+  privacy VARCHAR(10) NOT NULL DEFAULT 'public' CHECK (privacy IN ('public', 'private')),
+  creation_date TIMESTAMP NOT NULL
 );
 
--- Create the Followers table --
-CREATE TABLE flq.Followers (
-  ID SERIAL PRIMARY KEY,
-  FollowerID INT NOT NULL REFERENCES flq.Users(ID),
-  FollowingID INT NOT NULL REFERENCES flq.Users(ID),
-  DateFollowed TIMESTAMP NOT NULL
+-- Create the Follower table --
+CREATE TABLE flq.Follower (
+  id SERIAL PRIMARY KEY,
+  follower_id INT NOT NULL REFERENCES flq.User(id),
+  following_id INT NOT NULL REFERENCES flq.User(id),
+  follow_date TIMESTAMP NOT NULL
 );
 
--- Create the Communities table --
-CREATE TABLE flq.Communities (
-  ID SERIAL PRIMARY KEY,
-  Name VARCHAR(100) NOT NULL,
-  Description TEXT,
-  Privacy VARCHAR(10) NOT NULL DEFAULT 'public',
-  DateCreated TIMESTAMP NOT NULL
+-- Create the Community table --
+CREATE TABLE flq.Community (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  privacy VARCHAR(10) NOT NULL DEFAULT 'public' CHECK (privacy IN ('public', 'private')),
+  creation_date TIMESTAMP NOT NULL
 );
 
--- Create the Memberships table --
-CREATE TABLE flq.Memberships (
-  ID SERIAL PRIMARY KEY,
-  UserID INT NOT NULL REFERENCES flq.Users(ID),
-  CommunityID INT NOT NULL REFERENCES flq.Communities(ID),
-  DateJoined TIMESTAMP NOT NULL,
-  IsCreator BOOLEAN NOT NULL DEFAULT FALSE
+-- Create the Membership table --
+CREATE TABLE flq.Membership (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES flq.User(id),
+  community_id INT NOT NULL REFERENCES flq.Community(id),
+  join_date TIMESTAMP NOT NULL,
+  is_creator BOOLEAN NOT NULL DEFAULT FALSE
 );
 
--- Create the TaggedPhotos table --
-CREATE TABLE flq.TaggedPhotos (
-  ID SERIAL PRIMARY KEY,
-  PhotoID INT NOT NULL REFERENCES flq.Photos(ID),
-  TagID INT NOT NULL REFERENCES flq.Tags(ID)
+-- Create the TaggedPhoto table --
+CREATE TABLE flq.TaggedPhoto (
+  id SERIAL PRIMARY KEY,
+  photo_id INT NOT NULL REFERENCES flq.Photo(id),
+  tag_id INT NOT NULL REFERENCES flq.Tag(id)
 );
 
--- Create the AlbumPhotos table --
-CREATE TABLE flq.AlbumPhotos (
-  ID SERIAL PRIMARY KEY,
-  PhotoID INT NOT NULL REFERENCES flq.Photos(ID),
-  AlbumID INT NOT NULL REFERENCES flq.Albums(ID)
+-- Create the AlbumPhoto table --
+CREATE TABLE flq.AlbumPhoto (
+  id SERIAL PRIMARY KEY,
+  photo_id INT NOT NULL REFERENCES flq.Photo(id),
+  album_id INT NOT NULL REFERENCES flq.Album(id)
 );
 
 -- Create the UserHistory table --
 CREATE TABLE flq.UserHistory (
-  ID SERIAL PRIMARY KEY,
-  UserID INT NOT NULL REFERENCES flq.Users(ID),
-  Name VARCHAR(100) NOT NULL,
-  Email VARCHAR(255) NOT NULL,
-  Password VARCHAR(255) NOT NULL,
-  StartDate TIMESTAMP NOT NULL,
-  EndDate TIMESTAMP
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES flq.User(id),
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP
 );
